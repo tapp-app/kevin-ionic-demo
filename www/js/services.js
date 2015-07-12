cfc6a3291a6b5d502a1b513f42a8c79711cebf8e
Kevin Van Beek <kvnvanbeek@gmail.com>
2015-07-11 22:33:54 -0700

    initial commit

---
+++ www/js/services.js
@@ -0,0 +1,27 @@
+angular.module('starter.services', ['ngResource'])
+    .factory('Session', function ($resource) {
+        return $resource('http://localhost:5000/sessions/:sessionId');
+    })
+    .factory('socket', ['$rootScope', function ($rootScope) {
+        var socket = io.connect('http://localhost:3000');
+        return {
+            on: function (eventName, callback) {
+                socket.on(eventName, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        callback.apply(socket, args);
+                    });
+                });
+            },
+            emit: function (eventName, data, callback) {
+                socket.emit(eventName, data, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback.apply(socket, args);
+                        }
+                    });
+                })
+            }
+        };
+    }]);
